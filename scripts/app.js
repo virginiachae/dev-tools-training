7e7fbf5173904f3cb13d0dfc5075c45b512b7a9a
virginiachae <chae.virginia@gmail.com>
2016-10-26 16:41:29 -0700

    fixed bugs up to line 35

--- scripts/app.js
+++ scripts/app.js
@@ -7,36 +7,38 @@ $(document).ready(function(){
 	function addSubHeader() {
 		var subHeader = $('<small>&nbsp&nbspsquash the bugs!</small>')
 		$('h1').append(subHeader);
-	}
+	};
+	addSubHeader();
 
-	
+
 
 
 	// the form redirects when it should just update the count of "E"s
 	$('form').on('submit', function(e){
 		console.log('form submitted');
 		var input = $('input').eq(0).val();
-
+		e.preventDefault();
 		// should count occurrences of the letter "E"
 		// and update the display. instead always shows 0
-		for (var i = 0; i < 10; i++){
+		var count = 0;
+		for (var i = 0; i <input.length; i++){
 			// this loop doesn't find both es in  "supercalifragilisticexpialidocious"
-			var count = 0;
+
 			if (input[i] === "e"){
-				count = count +1;
+				count = count += 1;
 			}
 			$('#num-es').text(count);
 		}
 	});
 
 
-	// all the links alert 5 :(
+	// all the links alert 5
 	function addLinks () {
 	    var $link;
 	    var $ul = $('ul');
 	    for (var i=0; i<5; i++) {
 	        $link = $('<a href="#" class="btn btn-link"></a>');
-	        $link.html('Link '+i);
+	        $link.html('Link '+ i);
 	        $link.on('click', function () {
 	            alert(i);
 	        });
@@ -45,4 +47,4 @@ $(document).ready(function(){
     }
 
     addLinks();
-};
+});
